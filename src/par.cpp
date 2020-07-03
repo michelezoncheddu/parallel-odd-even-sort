4db4c895f002494576ce7dcfdb73d5da0bbddf13
michelezoncheddu <michelezoncheddu@users.noreply.github.com>
2020-07-03 12:36:22 +0200

    Fix thread work partitioning

--- src/par.cpp
+++ src/par.cpp
@@ -13,7 +13,7 @@ std::atomic<int> global_swaps = 0;
 
 template <typename T>
 int odd_even_sort(T *v, short phase, size_t end) {
-    int swaps = 0;
+    auto swaps{0};
     for (size_t i = phase; i < end; i += 2) {
         auto first = v[i], second = v[i + 1];
         auto cond = first > second;
@@ -36,20 +36,35 @@ void f(T *v, size_t end, barrier &barrier) {
     } while (global_swaps > 0);
 }
 
-int main() {
-    auto v = create_random_vector<vec_type>(N_ELEM, MIN, MAX, SEED);
+int main(int argc, char const *argv[]) {
+    if (argc < 3) {
+        std::cout << "Usage is " << argv[0]
+                  << " n nw" << std::endl;
+        return -1;
+    }
+
+    auto const n = strtol(argv[1], nullptr, 10);
+    auto const nw = strtol(argv[2], nullptr, 10);
+
+    auto v = create_random_vector<vec_type>(n, MIN, MAX, SEED);
     auto const ptr = v.data();
 
-    int const nw = 2;
     barrier barrier(nw);
-
-    size_t const chunk_len = v.size() / nw;
-
     std::vector<std::thread*> threads(nw);
 
+    size_t const chunk_len = v.size() / nw;
+    int remaining = static_cast<int>(v.size() % nw);
+    size_t offset = 0;
+
     auto const start_time = std::chrono::system_clock::now();
-    for (size_t i = 0, offset = 0; i < nw; ++i, offset += chunk_len)
-        threads[i] = new std::thread(f<vec_type>, ptr + offset, chunk_len, std::ref(barrier));
+    for (int i = 0; i < nw - 1; ++i) {
+        //std::cout << offset << ", " << chunk_len + (remaining > 0) << std::endl;
+        threads[i] = new std::thread(f<vec_type>, ptr + offset, chunk_len + (remaining > 0), std::ref(barrier));
+        offset += chunk_len + (remaining > 0);
+        --remaining;
+    }
+    //std::cout << offset << ", " << chunk_len - 1 << std::endl;
+    threads[threads.size() - 1] = new std::thread(f<vec_type>, ptr + offset, chunk_len - 1, std::ref(barrier));
 
     for (auto thread : threads)
         thread->join();
