e3a9822996623a68cff569dcb2b1dcf99da3f878
michelezoncheddu <michelezoncheddu@users.noreply.github.com>
2020-07-09 16:37:37 +0200

    Code cleaning

--- src/par.cpp
+++ src/par.cpp
@@ -9,12 +9,14 @@
 #include <config.hpp>
 #include <util.hpp>
 
+// Padding for the swap array
+unsigned constexpr padding = 64 / sizeof(unsigned); // TODO: read from input (at least)
+
 bool finished = false;
-unsigned constexpr padding = 64 / sizeof(unsigned);
 
 template <typename T>
-int odd_even_sort(T *v, short phase, size_t end) {
-    int swaps = 0;
+unsigned odd_even_sort(T * const v, short phase, size_t end) {
+    unsigned swaps = 0;
     for (size_t i = phase; i < end; i += 2) {
         auto first = v[i], second = v[i + 1];
         auto cond = first > second;
@@ -27,18 +29,21 @@ int odd_even_sort(T *v, short phase, size_t end) {
 }
 
 template <typename T>
-void f(unsigned thid, T *v, size_t end, int alignment, std::vector<std::unique_ptr<barrier>> &barriers1, std::vector<std::unique_ptr<barrier>> &barriers2, std::vector<unsigned> &swaps) {
-    int iter = 0;
-    int pos = thid * padding;
+void f(unsigned thid, T * const v, size_t end, int alignment,
+        std::vector<std::unique_ptr<barrier>> const &barriers1,
+        std::vector<std::unique_ptr<barrier>> const &barriers2,
+        std::vector<unsigned> &swaps) {
+    auto iter = 0;
+    auto pos = thid * padding;
     while (!finished) {
-        swaps[pos] |= (unsigned)odd_even_sort(v, !alignment, end);
+        swaps[pos] |= odd_even_sort(v, !alignment, end); // Odd phase
         barriers1[iter]->wait();
-        swaps[pos] |= (unsigned)odd_even_sort(v, alignment, end);
+        swaps[pos] |= odd_even_sort(v, alignment, end); // Even phase
         barriers2[iter++]->wait();
     }
 }
 
-void controller_body(std::vector<std::unique_ptr<barrier>> &barriers, std::vector<unsigned> &swaps) {
+void controller_body(std::vector<std::unique_ptr<barrier>> const &barriers, std::vector<unsigned> &swaps) {
     int iter = 0;
     while (true) {
         unsigned tmp = 0;
@@ -82,14 +87,16 @@ int main(int argc, char const *argv[]) {
 
     auto const start_time = std::chrono::system_clock::now();
 
-    std::vector<std::unique_ptr<barrier>> barriers1(n), barriers2(n);
+    // TODO: rename variables
+    std::vector<std::unique_ptr<barrier>> barriers1(n), barriers2(n); // n is an upper bound for the number of iterations
     for (auto &elem : barriers1)
         elem = std::make_unique<barrier>(nw);
     for (auto &elem : barriers2)
-        elem = std::make_unique<barrier>(nw + 1);
+        elem = std::make_unique<barrier>(nw + 1); // plus the controller
 
     std::vector<std::unique_ptr<std::thread>> threads;
     threads.reserve(nw);
+
     std::vector<unsigned> swaps(nw * padding, 0);
 
     size_t const chunk_len = v.size() / nw;
@@ -100,14 +107,16 @@ int main(int argc, char const *argv[]) {
 
     for (unsigned i = 0; i < nw - 1; ++i) {
         threads.push_back(std::make_unique<std::thread>(
-                f<vec_type>, i, ptr + offset, chunk_len + (remaining > 0), offset % 2, std::ref(barriers1), std::ref(barriers2), std::ref(swaps)));
+                f<vec_type>, i, ptr + offset, chunk_len + (remaining > 0), offset % 2,
+                std::ref(barriers1), std::ref(barriers2), std::ref(swaps)));
         offset += chunk_len + (remaining > 0);
         --remaining;
     }
     threads.push_back(std::make_unique<std::thread>(
-            f<vec_type>, nw - 1, ptr + offset, chunk_len - 1, offset % 2, std::ref(barriers1), std::ref(barriers2), std::ref(swaps)));
+            f<vec_type>, nw - 1, ptr + offset, chunk_len - 1, offset % 2,
+            std::ref(barriers1), std::ref(barriers2), std::ref(swaps)));
 
-    // Thread pinning (TODO: hw concurrency awareness)
+    // Thread pinning (works only on Linux) (TODO: hw concurrency awareness)
     cpu_set_t cpuset;
     for (auto i = 0; i < nw; ++i) {
         CPU_ZERO(&cpuset);
--- src/seq.cpp
+++ src/seq.cpp
@@ -9,8 +9,8 @@
 #include <util.hpp>
 
 template <typename T>
-int odd_even_sort(std::vector<T> &v, int start) {
-    auto swaps = 0;
+unsigned odd_even_sort(std::vector<T> &v, int start) {
+    unsigned swaps = 0;
     for (size_t i = start; i < v.size() - 1; i += 2) {
         auto first = v[i], second = v[i + 1];
         auto cond = first > second;
@@ -29,14 +29,14 @@ int main(int argc, char const *argv[]) {
         return -1;
     }
 
-    auto thread_id = std::this_thread::get_id();
-    auto native_handle = *reinterpret_cast<std::thread::native_handle_type*>(&thread_id);
-
     auto const n = strtol(argv[1], nullptr, 10);
 
     auto v = create_random_vector<vec_type>(n, MIN, MAX, SEED);
-    int swaps;
+    unsigned swaps;
 
+    // Dirty trick for get the current thread handle
+    auto thread_id = std::this_thread::get_id();
+    auto native_handle = *reinterpret_cast<std::thread::native_handle_type*>(&thread_id);
     cpu_set_t cpuset;
     CPU_ZERO(&cpuset);
     CPU_SET(0, &cpuset);
@@ -47,8 +47,8 @@ int main(int argc, char const *argv[]) {
 
     auto start = std::chrono::system_clock::now();
     do {
-        swaps  = odd_even_sort(v, 1); // odd
-        swaps += odd_even_sort(v, 0); // even
+        swaps  = odd_even_sort(v, 1); // Odd phase
+        swaps += odd_even_sort(v, 0); // Even phase
     } while (swaps > 0);
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
             std::chrono::system_clock::now() - start).count();
