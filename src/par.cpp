108a8dcb59afcd84ab6f9dc7aa04aa46c76df221
michelezoncheddu <michelezoncheddu@users.noreply.github.com>
2020-07-13 16:19:51 +0200

    ASM optimizations

--- src/par.cpp
+++ src/par.cpp
@@ -12,6 +12,8 @@
 // Padding for the swap array
 unsigned constexpr padding = 64 / sizeof(unsigned); // TODO: read from input (at least)
 
+unsigned nw;
+
 bool finished = false;
 
 template <typename T>
@@ -30,20 +32,27 @@ unsigned odd_even_sort(T * const v, short phase, size_t end) {
 
 template <typename T>
 void thread_body(unsigned thid, T * const v, size_t end, int alignment,
-        std::vector<std::unique_ptr<barrier>> const &barriers1,
-        std::vector<std::unique_ptr<barrier>> const &barriers2,
-        std::vector<unsigned> &swaps) {
+        std::vector<unsigned> &phases,
+        std::vector<unsigned> &swaps,
+        std::vector<std::unique_ptr<barrier>> const &barriers) {
     auto iter = 0;
     auto const pos = thid * padding;
+    auto const has_left_neigh = thid > 0, has_right_neigh = thid < nw - 1;
     while (!finished) {
         swaps[pos] |= odd_even_sort(v, !alignment, end); // Odd phase
-        barriers1[iter]->wait();
+        phases[thid * padding]++;
+        if (has_right_neigh)
+            while (phases[thid * padding] != phases[(thid + 1) * padding])
+                __asm__("nop");
+        if (has_left_neigh)
+            while (phases[thid * padding] != phases[(thid - 1) * padding])
+                __asm__("nop");
         swaps[pos] |= odd_even_sort(v, alignment, end); // Even phase
-        barriers2[iter++]->wait();
+        barriers[iter++]->wait();
     }
 }
 
-void controller_body(std::vector<std::unique_ptr<barrier>> const &barriers, std::vector<unsigned> &swaps) {
+void controller_body(std::vector<unsigned> &swaps, std::vector<std::unique_ptr<barrier>> const &barriers) {
     auto iter = 0;
     while (true) {
         unsigned tmp = 0;
@@ -52,12 +61,10 @@ void controller_body(std::vector<std::unique_ptr<barrier>> const &barriers, std:
                 tmp |= swaps[i];
         }
 
-        if (!tmp) { // TODO: needed? Test performance.
-            size_t i = 0;
-            while (!tmp && i < swaps.size()) {
-                tmp |= swaps[i];
-                i += padding;
-            }
+        size_t i = 0;
+        while (!tmp && i < swaps.size()) {
+            tmp |= swaps[i];
+            i += padding;
         }
 
         if (!tmp) {
@@ -80,7 +87,7 @@ int main(int argc, char const *argv[]) {
     }
 
     auto const n  = strtol(argv[1], nullptr, 10);
-    auto const nw = strtol(argv[2], nullptr, 10);
+    nw = strtol(argv[2], nullptr, 10);
 
     auto v = create_random_vector<vec_type>(n, MIN, MAX, SEED);
     auto const ptr = v.data();
@@ -88,11 +95,11 @@ int main(int argc, char const *argv[]) {
     auto const start_time = std::chrono::system_clock::now();
 
     // TODO: rename variables
-    std::vector<std::unique_ptr<barrier>> barriers1(n), barriers2(n); // n is an upper bound for the number of iterations
-    for (auto &elem : barriers1)
-        elem = std::make_unique<barrier>(nw);
-    for (auto &elem : barriers2)
-        elem = std::make_unique<barrier>(nw + 1); // plus the controller
+    std::vector<std::unique_ptr<barrier>> barriers(n); // n is an upper bound for the number of iterations
+    for (auto &elem : barriers)
+        elem = std::make_unique<barrier>(nw + 1); // + 1 for the controller
+
+    std::vector<unsigned> phases(nw * padding, 0);
 
     std::vector<std::unique_ptr<std::thread>> threads;
     threads.reserve(nw);
@@ -103,22 +110,22 @@ int main(int argc, char const *argv[]) {
     int remaining = static_cast<int>(v.size() % nw);
     size_t offset = 0;
 
-    std::thread controller(controller_body, std::ref(barriers2), std::ref(swaps));
+    std::thread controller(controller_body, std::ref(swaps), std::ref(barriers));
 
-    for (auto i = 0; i < nw - 1; ++i) {
+    for (unsigned i = 0; i < nw - 1; ++i) {
         threads.push_back(std::make_unique<std::thread>(
                 thread_body<vec_type>, i, ptr + offset, chunk_len + (remaining > 0), offset % 2,
-                std::ref(barriers1), std::ref(barriers2), std::ref(swaps)));
+                std::ref(phases), std::ref(swaps), std::ref(barriers)));
         offset += chunk_len + (remaining > 0);
         --remaining;
     }
     threads.push_back(std::make_unique<std::thread>(
             thread_body<vec_type>, nw - 1, ptr + offset, chunk_len - 1, offset % 2,
-            std::ref(barriers1), std::ref(barriers2), std::ref(swaps)));
+            std::ref(phases), std::ref(swaps), std::ref(barriers)));
 
     // Thread pinning (works only on Linux) (TODO: hw concurrency awareness)
     cpu_set_t cpuset;
-    for (auto i = 0; i < nw; ++i) {
+    for (unsigned i = 0; i < nw; ++i) {
         CPU_ZERO(&cpuset);
         CPU_SET(i, &cpuset);
         if (0 != pthread_setaffinity_np(threads[i]->native_handle(), sizeof(cpu_set_t), &cpuset)) {
